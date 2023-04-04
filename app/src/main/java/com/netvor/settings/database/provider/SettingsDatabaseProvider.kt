f1b9eb61599402505dce18ca4cdac26b1b8319e2
gamemn02 <2udcjx@gmail.com>
2023-04-04 07:02:47 +0100

    Add an uri path to check weather package write secure permission is granted

--- app/src/main/java/com/netvor/settings/database/provider/SettingsDatabaseProvider.kt
+++ app/src/main/java/com/netvor/settings/database/provider/SettingsDatabaseProvider.kt
@@ -1,11 +1,16 @@
 package com.netvor.settings.database.provider
 
+import android.Manifest
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.pm.PackageManager
 import android.database.Cursor
+import android.database.MatrixCursor
 import android.net.Uri
+import androidx.core.content.ContextCompat
 
 private const val SETTINGS_URI_AUTHORITY = "settings"
+private const val WRITE_SECURE_PERMISSION_PATH = "/writeSecurePermission"
 private const val PERMISSION = "com.netvor.settings.database.provider.permission.SETTINGS_DATABASE_PROVIDER"
 
 class SettingsDatabaseProvider : ContentProvider() {
@@ -30,7 +35,10 @@ class SettingsDatabaseProvider : ContentProvider() {
         uri: Uri, projection: Array<String>?, selection: String?,
         selectionArgs: Array<String>?, sortOrder: String?
     ): Cursor? {
-        return context?.contentResolver?.query(uri.buildUpon().authority(SETTINGS_URI_AUTHORITY).build(), projection, selection, selectionArgs, sortOrder)
+        return if (uri.path == WRITE_SECURE_PERMISSION_PATH)
+            MatrixCursor(arrayOf("granted")).apply { addRow(arrayOf(if (checkWriteSecureSettingsPermission()) 1 else 0)) }
+        else
+            context?.contentResolver?.query(uri.buildUpon().authority(SETTINGS_URI_AUTHORITY).build(), projection, selection, selectionArgs, sortOrder)
     }
 
     override fun update(
@@ -39,4 +47,10 @@ class SettingsDatabaseProvider : ContentProvider() {
     ): Int {
         return context?.contentResolver?.update(uri.buildUpon().authority(SETTINGS_URI_AUTHORITY).build(), values, selection, selectionArgs) ?: 0
     }
+
+    private fun checkWriteSecureSettingsPermission() =
+        ContextCompat.checkSelfPermission(
+            context ?: throw (NullPointerException("Context should already set but is null!")),
+            Manifest.permission.WRITE_SECURE_SETTINGS
+        ) == PackageManager.PERMISSION_GRANTED
 }
